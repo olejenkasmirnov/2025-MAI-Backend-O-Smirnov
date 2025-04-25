bebd05cf46d4916a3e56b03885acc637b75f4c4a
olejenkasmirnov <mr.smirnov.oleg2001@mail.ru>
2025-04-25 19:52:05 +0300

    Add hw1: LRUCache implementation and test

---
+++ hw1/LRU_Cache.py
@@ -0,0 +1,32 @@
+class LRUCache:
+    def __init__(self, capacity: int = 10) -> None:
+        self.capacity = capacity
+        self.cache = {}
+        self.order = {}
+        self.counter = 0
+
+    def get(self, key: str) -> str:
+        if key in self.cache:
+            self.order[key] = self.counter
+            self.counter += 1
+            return self.cache[key]
+        return ""
+
+    def set(self, key: str, value: str) -> None:
+        if key in self.cache:
+            self.cache[key] = value
+            self.order[key] = self.counter
+            self.counter += 1
+        else:
+            if len(self.cache) >= self.capacity:
+                oldest_key = min(self.order, key=lambda k: self.order[k])
+                del self.cache[oldest_key]
+                del self.order[oldest_key]
+            self.cache[key] = value
+            self.order[key] = self.counter
+            self.counter += 1
+
+    def rem(self, key: str) -> None:
+        if key in self.cache:
+            del self.cache[key]
+            del self.order[key]
---
+++ hw1/LRU_Test.py
@@ -0,0 +1,13 @@
+from LRU_Cache import LRUCache
+
+def main():
+    cache = LRUCache(100)
+    cache.set('Jesse', 'Pinkman')
+    cache.set('Walter', 'White')
+    cache.set('Jesse', 'James')
+    print(cache.get('Jesse'))  # 'James'
+    cache.rem('Walter')
+    print(cache.get('Walter'))  # ''
+
+if __name__ == "__main__":
+    main()
